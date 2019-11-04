c1afd6f7276b2f62a85e53820cd7ea4b29925df1
panamacheng <toptalent.org@outlook.com>
2019-11-04 06:52:03 -0800

    updated user management

--- controllers/admin/usersController.js
+++ controllers/admin/usersController.js
@@ -1,12 +1,68 @@
+var bcrypt = require('bcrypt-nodejs');
+var UserModel = require('../../models').User;
+
 var usersController = {
   createAccount: (req, res) => {
-    console.log(req.body);
+    var newAccount = {
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password,
+      role: 'user',
+    };
+
+    UserModel.create(newAccount)
+      .then(result => {
+        res.status(200),json({
+          flag: true,
+          data: result.get()
+        });
+      })
+      .catch(error => {
+        console.log(error)
+        res.status(401).json({
+          flag: false,
+          data: error,
+        })
+      });
   },
   changePassword: (req, res) => {
-
+    var updateData = {
+      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
+    }
+    UserModel.update(updateData, {
+      where: {
+        id: req.body.id,
+      }
+    }).then(result => {
+      res.status(200).json({
+        flag: true,
+        data: result
+      })
+    }).catch(error => {
+      console.log(error);
+      res.status(401).json({
+        flag: false,
+        data: error,
+      })
+    });
   },
   deleteAccount: (req, res) => {
-
+    UserModel.destroy({
+      where: {
+        id: req.body.id
+      }
+    }).then(() => {
+      res.status(200).json({
+        flag: true,
+        data: 'Deleted',
+      })
+    }).catch(error => {
+      console.log(error);
+      res.status(401).json({
+        flag: false,
+        data: error,
+      })
+    });
   }
 }
 
--- public/app/admin/users.js
+++ public/app/admin/users.js
@@ -1,33 +1,111 @@
 $(document).ready(function () {
+  mkNotifications({
+    positionY: 'top',
+    positionX: 'right',
+    max: 15,
+    scrollable: false
+  });
+
   this.createUser = function () {
     var method = "POST";
     var url = "/admin/users";
-    var sendData = {};
+    var sendData = {
+      userName: $('input#userName').val(),
+      email: $('input#email').val(),
+      password: $('input#password').val(),
+    };
 
     returnData(method, url, sendData, callback => {
+      if (callback && callback.flag == true) {
+        mkNoti(
+          'Success!',
+          'Created the user account',
+          {
+              status:'success'
+          }
+        );
 
+        setTimeout(() => {
+          window.open('/users', '_self');
+        }, 300);
+      } else {
+        mkNoti(
+          'Warning!',
+          'Network error!',
+          {
+              status:'warning'
+          }
+        );  
+      }
     });
   }
 
   this.deleteUser = function(id) {
     var method = "DELETE";
-    var url = "/admin/users/:"+id;
-    var sendData = {};
+    var url = "/admin/users";
+    var sendData = {
+      id: id,
+    };
 
     returnData(method, url, sendData, callback => {
+      if (callback && callback.flag == true) {
+        mkNoti(
+          'Success!',
+          'Deleted the user account',
+          {
+              status:'success'
+          }
+        );
 
+        setTimeout(() => {
+          window.open('/users', '_self');
+        }, 300);
+      } else {
+        mkNoti(
+          'Warning!',
+          'Network error!',
+          {
+              status:'warning'
+          }
+        );  
+      }
     });
   }
 
   this.changePassword = function(id) {
-    var method = "PUT";
-    var url = "/admin/users/:"+id;
-    var sendData = {
-      password: "",
+    if ($('input#cpassword'+id).val() == $('input#crpassword'+id).val()) {
+      var method = "PUT";
+      var url = "/admin/users";
+      var sendData = {
+        id: id,
+        password: $('input#cpassword'+id).val(),
+      }
+
+      returnData(method, url, sendData, callback => {
+        if (callback && callback.flag == true) {
+          mkNoti(
+            'Success!',
+            'Changed user password',
+            {
+                status:'success'
+            }
+          );
+
+          setTimeout(() => {
+            window.open('/users', '_self');
+          }, 300);
+        }
+      });
+    } else {
+      mkNoti(
+        'Warning!',
+        'Input the same password',
+        {
+            status:'warning'
+        }
+      );
+
+      $('input#cpassword'+id).focus();
     }
-
-    returnData(method, url, sendData, callback => {
-
-    });
   }
 });
